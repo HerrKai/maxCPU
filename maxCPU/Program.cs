f2d4fcb4b38c22c7100a787959334f3c1ae771a6
Kai <HerrKai@users.noreply.github.com>
2019-11-09 22:45:52 +0100

    init

---
+++ maxCPU/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace maxCPU
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(isPrimeNumber(7));
+            Console.ReadKey();
+        }
+        static bool isPrimeNumber(int argTarget)
+        {
+            bool isPrime = true;
+            for (int i = 2; i < argTarget; i++)
+            {
+                if (argTarget % i == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+            return isPrime;
+        }
+    }
+}
